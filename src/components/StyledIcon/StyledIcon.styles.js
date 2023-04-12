bb17b82e17e8da585e65a7b77e2a9888b4bcb0b1
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-04-12 20:12:59 +0200

    update assets, Navigation and StyledIcon

--- src/components/StyledIcon/StyledIcon.styles.js
+++ src/components/StyledIcon/StyledIcon.styles.js
@@ -2,19 +2,28 @@ import styled from "styled-components";
 
 
 export const StyledIconWrapper = styled.span`
+  
+  
     
     background-color: palevioletred;
 
   display: inline-block;
+  margin: 0 20px;
+  width: 50px;
+  height: 50px;
 
   svg {
     width: 100%;
-    height: auto;
   }
 
   svg path {
-    fill: ${({ isDark }) => (isDark ? '#000' : '#fff')};
+    fill: ${({ isDark }) => (isDark ? '#FD0909' : '#fff')};
   }
-    
+
+  ${({ theme }) => theme.mq.desktop} {
+    width: 25px;
+    height: 25px;
+    margin: 0 15px;
+  }
     
     `
