eeb15b550ffac07727b42dd865053df2722ebf71
Kedmar20 <m.kedzierski76@gmail.com>
2023-04-17 12:57:24 +0200

    update page styles 'Leistungen'

---
+++ src/assets/styles/pages/leistungen.styles.js
@@ -0,0 +1,127 @@
+import styled from 'styled-components';
+// import arrowIcon from '../../icons/arrow.svg';
+import arrowIcon from 'assets/icons/blocks.svg';
+import { StyledList } from '../../../components/StyledList/StyledList.styles';
+
+export const Hero = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 80vh;
+  margin-bottom: 100px;
+
+  h1 {
+    margin: 0;
+    font-size: ${({ theme }) => theme.font.size.heading};
+  }
+  
+  ${({ theme }) => theme.mq.tablet} {
+    height: 40vh;
+  }
+  
+  ${({ theme }) => theme.mq.desktop} {
+    height: ${({ theme }) => theme.size.desktopHeroHeight};
+    justify-content: flex-end;
+    
+    h1 {
+      background-color: hsla(0, 100%, 100%, 0.6);
+      padding: 30px 20px;
+      max-width: 550px;
+      margin-bottom: 380px;
+    }
+  }
+`;
+
+export const HeroInfo = styled.div`
+  a {
+    color: black;
+    font-weight: 700;
+    position: relative;
+    left: 30px;
+
+    &::before {
+      position: absolute;
+      content: '';      
+      background-image: url("${arrowIcon}");
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-position: 0 50%;
+      width: 20px;
+      height: 20px;
+      left: -30px;
+      top: 50%;
+      transform: translateY(-50%) rotate(90deg);
+      //border: red solid 1px;
+      color: red;
+    }
+  }
+  
+  ${({ theme }) => theme.mq.desktop} {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-direction: row-reverse;
+    
+    &::before {
+      content: '';
+      height: 1px;
+      width: 100%;
+      position: absolute;
+      background-color: ${({ theme }) => theme.colors.steel};
+      transform: translateY(-50px);
+      border: green solid 1px;
+    }
+    
+    p {
+      margin: 0;
+      width: 460px;
+    }
+  }
+  
+  
+`;
+
+export const HeroImage = styled.div`
+  background-image: url("${({ imageSource }) => imageSource}");
+  background-position: 50% 50%;
+  background-size: cover;
+  width: 90%;
+  height: 60%;
+  position: absolute;
+  z-index: -1;
+  top: 15%;
+  right: 0;
+  
+  ${({ theme }) => theme.mq.tablet} {
+    width: 70%;
+  }
+  
+  ${({ theme }) => theme.mq.desktop} {
+    top: 0;
+    width: 75%;
+    height: 75%;
+  }
+`;
+
+export const ServicesStyledList = styled(StyledList)`
+  li {
+    margin: 80px 0;
+    max-width: 500px;
+  }
+
+  svg {
+    margin-bottom: 25px;
+  }
+  
+  ${({ theme }) => theme.mq.desktop} {
+    grid-template-rows: repeat(2, 300px)
+  }
+`;
+
+export const ContentWrapperLeistungen = styled.div`
+{
+  margin: 90px 0;
+  padding: 0 15px;
+}
+    `;
---
+++ src/pages/leistungen.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import BlocksIcon from 'assets/icons/blocks.svg';
+import ConsultancyIcon from 'assets/icons/consultancy.svg';
+import HeartIcon from 'assets/icons/heart.svg';
+import KeyIcon from 'assets/icons/key.svg';
+import PeopleIcon from 'assets/icons/people.svg';
+import SuitcaseIcon from 'assets/icons/suitcase.svg';
+import PenIcon from 'assets/icons/pen.svg';
+import { graphql } from 'gatsby';
+import {
+    Hero, HeroImage, HeroInfo, ServicesStyledList, ContentWrapperLeistungen
+} from 'assets/styles/pages/leistungen.styles';
+import { HighlightedHeading } from '../components/HighlightedHeading/HighlightedHeading';
+
+const Leistungen = ({ data }) => (
+    <ContentWrapperLeistungen>
+        <Hero>
+            <h1>Full-Service-Büro für alle Anliegen des Immobilienverkaufs</h1>
+            <HeroImage imageSource={data.leistungen.publicURL} />
+            <HeroInfo>
+                <p>
+                    Freundlicher Kundenkontakt auf Augenhöhe und modernste Vermarktungsmöglichkeiten sind uns besonders wichtig.
+                </p>
+                <a href="#details">Weiterlesen</a>
+            </HeroInfo>
+        </Hero>
+        <HighlightedHeading id="details">Wie können wir Ihnen helfen?</HighlightedHeading>
+        <ServicesStyledList>
+            <li>
+                <PeopleIcon />
+                <h3>Bewertung</h3>
+                <p>
+                    Realistische Immobilien-Bewertung für beste Erfolgschancen. Immobilie einfach kostenlos bewerten lassen.
+                </p>
+            </li>
+            <li>
+                <ConsultancyIcon />
+                <h3>Beratung</h3>
+                <p>
+                    Kostenlose Immobilienberatung. Wichtige Fragen vor dem Verkauf klären. Wie is die aktuelle Situation am Markt. Tipps / Rat vom Experten.
+                    Proffesionelle Einschätzung.
+
+                </p>
+            </li>
+            <li>
+                <HeartIcon />
+                <h3>Home staging</h3>
+                <p>
+                    Für den ersten Eindruck gibt es keine zweite Chance. Wir beraten Sie gerne während eines persönlichen Besichtigungstermins und zeigen Ihnen Möglichkeiten und Vorschläge für eine optimale Präsentation Ihrer Immobilie mit Home Staging.
+                </p>
+            </li>
+            <li>
+                <BlocksIcon />
+                <h3>Suchauftrag</h3>
+                <p>
+                    Sie suchen eine Immobilie in der Region Freiburg zum Kauf? Lassen Sie sich jetzt kostenlos und unverbindlich in unsere Such-Kartei aufnehmen und bleiben Sie über neue Immobilien informiert.
+                </p>
+            </li>
+            <li>
+                <SuitcaseIcon />
+                <h3>Top Preis-Leistung und keinerei Risiko</h3>
+                <p>
+                    Sie vertrauen uns Ihre Immobilie an und wir vertrauen auf eine gute Zusammenarbeit. Als Maler Ihres Vertrauens gehen wir für einen erfolgreichen Verkaufsabschluss mit allem in Vorleistung. Das „Rundum-Sorglos-Paket“ beinhaltet all unsere Services und ...
+                </p>
+            </li>
+            <li>
+                <PenIcon />
+                <h3>Unterlagen</h3>
+                <p>
+                    Komplette Unterlagen-Beschaffung inkl. Bankenpaket.
+
+
+                </p>
+            </li>
+        </ServicesStyledList>
+    </ContentWrapperLeistungen>
+);
+
+export const query = graphql`
+    query {
+        leistungen: file(relativePath: {regex: "/leistungen\/leistungen.jpg/"}) {
+            publicURL
+        },
+    }`;
+
+export default Leistungen;
